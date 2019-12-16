75c81a4ed567e699bf47ad6877ae04af744263da
weisong <weisongyap@gmail.com>
2019-12-16 11:37:03 +0800

    7.23 Higher Order Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
   constructor(props) {
@@ -79,7 +80,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -96,7 +97,7 @@ class App extends Component {
           ></Cockpit>
         ) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
